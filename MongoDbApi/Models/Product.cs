2f9c87eecc69482b963db9c6193c4ab3380853ba
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-05-29 11:22:57 -0600

    Added Product class

---
+++ MongoDbApi/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDbApi.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Stock { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string Category { get; set; }
+    }
+}
