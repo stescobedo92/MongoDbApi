df877c353bb7b03f031b2589e6c11732e621fb47
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-05-29 11:45:46 -0600

    Added MongoDBRepository class

---
+++ MongoDbApi/Repositories/MongoDBRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace MongoDbApi.Repositories
+{
+    public class MongoDBRepository
+    {
+        public MongoClient client;
+
+        public IMongoDatabase db;
+
+        public MongoDBRepository()
+        {
+            client = new MongoClient("mongodb://127.0.0.1:27017");
+            db = client.GetDatabase("Inventory");
+        }
+    }
+}
