1fb92a777a7bd22adcabb5ddd018edf6826fe96b
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-05-29 11:24:28 -0600

    Added property for Id in mongo data base

--- MongoDbApi/Models/Product.cs
+++ MongoDbApi/Models/Product.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 
 namespace MongoDbApi.Models
 {
     public class Product
     {
+        public ObjectId Id { get; set; }
         public string Name { get; set; }
         public string Stock { get; set; }
         public DateTime ExpiryDate { get; set; }
