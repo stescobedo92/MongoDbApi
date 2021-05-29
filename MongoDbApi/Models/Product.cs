e16366e3ba9cde12d7ccaa716ac8006dd108e383
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-05-29 11:25:31 -0600

    Added BsonId attribute

--- MongoDbApi/Models/Product.cs
+++ MongoDbApi/Models/Product.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace MongoDbApi.Models
 {
     public class Product
     {
+        [BsonId]
         public ObjectId Id { get; set; }
         public string Name { get; set; }
         public string Stock { get; set; }
