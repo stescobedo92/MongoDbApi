5206b5c24e55553e1b762a1c4cfe691d6b1a382b
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-05-29 11:51:07 -0600

    Added IProductCollection interface for create, update, delete and list all products

---
+++ MongoDbApi/Repositories/IProductCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDbApi.Models;
+
+namespace MongoDbApi.Repositories
+{
+    public interface IProductCollection
+    {
+        Task InsertProduct(Product product);
+        Task UpdateProduct(Product product);
+        Task DeleteProduct(string id);
+
+        Task<List<Product>> GetAllProducts();
+        Task<Product> GetProductById(string id);
+    }
+}
