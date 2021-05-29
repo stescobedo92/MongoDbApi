31cbeac5edcfe9904b8e7ee69299ae10a9673324
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-05-29 13:06:22 -0600

    Implement CRUD

---
+++ MongoDbApi/Repositories/ProductCollection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDbApi.Models;
+
+namespace MongoDbApi.Repositories
+{
+    public class ProductCollection : IProductCollection
+    {
+        //Added the mongodb repository
+        internal MongoDBRepository _mongoDBRepository = new MongoDBRepository();
+        private IMongoCollection<Product> Collection;
+
+        public ProductCollection()
+        {
+            Collection = _mongoDBRepository.db.GetCollection<Product>("Products");
+        }
+
+        public async Task DeleteProduct(string id)
+        {
+            var filter = Builders<Product>.Filter.Eq(s => s.Id, new ObjectId(id));
+            await Collection.DeleteOneAsync(filter);
+        }
+
+        public async Task<List<Product>> GetAllProducts()
+        {
+            return await Collection.FindAsync(new BsonDocument()).Result.ToListAsync();
+        }
+
+        public async Task<Product> GetProductById(string id)
+        {
+            return await Collection.FindAsync(new BsonDocument { { "_id", new ObjectId(id) } }).Result.FirstAsync();
+        }
+
+        public async Task InsertProduct(Product product)
+        {
+            await Collection.InsertOneAsync(product);
+        }
+
+        public async Task UpdateProduct(Product product)
+        {
+            var filter = Builders<Product>.Filter.Eq(s => s.Id, product.Id);
+
+            await Collection.ReplaceOneAsync(filter, product);
+        }
+    }
+}
